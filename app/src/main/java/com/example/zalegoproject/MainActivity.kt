0e2a81f7a037433c5741f8c03141072d5a0d6b64
mocohdee <mocohdee01@gmail.com>
2022-09-29 23:26:14 +0300

    final commit

---
+++ app/src/main/java/com/example/zalegoproject/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.zalegoproject
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+              val intent = Intent(this@MainActivity, splashScreen::class.java)
+            startActivity(intent)
+        }, 3000)
+
+
+    }
+}
